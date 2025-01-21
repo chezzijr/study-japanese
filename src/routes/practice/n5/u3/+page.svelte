733981ea2e8912fcb66976acc07ba806a3477843
chezzijr <nhuthuan0501@gmail.com>
2025-01-21 22:45:27 +0700

    add u3 u4 and reroute

---
+++ src/routes/practice/n5/u3/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import MCQ from '$lib/components/mcq.svelte';
+    import U3 from '$lib/n5/u3.json';
+
+    const quesList = Object.keys(U3);
+    const ansList = Object.values(U3).map((v) => v.vietnamese);
+    const hintList = Object.values(U3).map((v) => v.phonetic);
+</script>
+
+<MCQ quesList={quesList} ansList={ansList} hintList={hintList} keyName="Từ/cụm từ" valueName="Tiếng Việt" />
+
