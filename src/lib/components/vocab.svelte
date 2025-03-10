3503f21365aa986370c1caa70e220b08518a6a22
chezzijr <nhuthuan0501@gmail.com>
2025-03-10 20:34:32 +0700

    add u10

--- src/lib/components/vocab.svelte
+++ src/lib/components/vocab.svelte
@@ -1,30 +1,20 @@
 <script lang="ts">
     let { words, phonetics, meanings, notes } = $props()
-
-    function focusRow(e: MouseEvent) {
-        const tr = e.target as HTMLTableRowElement;
-        tr.classList.add('bg-base-200');
-    }
-
-    function unfocusRow(e: MouseEvent) {
-        const tr = e.target as HTMLTableRowElement;
-        tr.classList.remove('bg-base-200');
-    }
 </script>
 
-<div class="overflow-x-auto">
+<div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
     <table class="table table-md table-pin-rows table-pin-cols text-center">
         <thead>
             <tr>
-                <th>Từ</th>
-                <th>Phiên âm Hiragana</th>
-                <th>Nghĩa</th>
-                <th>Ghi chú</th>
+                <td>Từ</td>
+                <td>Phiên âm Hiragana</td>
+                <td>Nghĩa</td>
+                <td>Ghi chú</td>
             </tr>
         </thead>
         <tbody>
             {#each words as word, i}
-                <tr onmouseenter={focusRow} onmouseleave={unfocusRow}>
+                <tr class="hover:bg-base-300">
                     <td>{word}</td>
                     <td>{phonetics[i]}</td>
                     <td>{meanings[i]}</td>
